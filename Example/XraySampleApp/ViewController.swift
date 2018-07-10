5183c2e5f048ce5a70a01ddab315a6b7a6246d6e
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2018-07-10 14:38:27 +0200

    Replaced DataServiceDelegate by closures API

--- Example/XraySampleApp/ViewController.swift
+++ Example/XraySampleApp/ViewController.swift
@@ -17,7 +17,19 @@ class ViewController: UIViewController {
         
         Xray.events.register(transmitter: MockTrasnmitter(behaviour: .retry(nextRetryAt: Date())))
         
+        Xray.data.queue = .main
         
+        Xray.data.onTrigger = { payload in
+            if let myData = String(data: payload.data, encoding: .utf8) {
+                
+                let controller = UIAlertController(title: "Data received", message: myData, preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "Ok", style: .default) { _ in
+                    self.dismiss(animated: true)
+                })
+                
+                self.present(controller, animated: true)
+            }
+        }
     }
     
     func hello() -> String {
