0308b34624397b66626b397d4289e2a4902bd613
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2018-07-11 14:08:38 +0200

    Definition of DataPayloadFilter protocol

---
+++ XrayKit/Sources/DataPayloadFilter.swift
@@ -0,0 +1,45 @@
+//
+//  This file is part of Xray SDK.
+//  See the file LICENSE.txt for copying permission.
+//
+
+import Foundation
+
+/**
+ A `DataPayloadFilter` takes in a list of `DataPayload`s and an `Event` and filters out the payloads that match and those that don't.
+ The payloads that match are candidates to be executed.
+ The payloads that don't can be either discarded completely or kept.
+*/
+protocol DataPayloadFilter {
+  
+    /**
+     Applies the filter to a list of `DataPayload` for an occured event.
+     
+     - parameter payloads: The list of payloads that already match the event name
+     - parameter event: The event that was used to querry by name all the payloads
+     
+     - warning: It is assumed that all payloads do have an `EventTrigger` with the only name matching the passed event.
+     */
+    func apply(to payloads: [DataPayload], withEvent event: Event) -> FilterResult
+}
+
+
+struct FilterResult {
+    let matched: [DataPayload]
+    let mismatched: [Mismatch]
+}
+
+/**
+ Represents a payload that did not match a filter and what should be done with it
+ */
+struct Mismatch {
+    
+    /// How to handle the payload.
+    enum Handle {
+        case keep       /// Keep the payload for the next time
+        case delete     /// Delete the payload
+    }
+    
+    let payload: DataPayload
+    let handle: Handle
+}
